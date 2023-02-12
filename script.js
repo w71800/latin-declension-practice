f8467b58c39dc990c561bf1d252efe3324e6f025
IKaRos <w71800@gmail.com>
2023-02-12 13:47:50 +0800

    bug in initData() fixed.

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
 import { url } from "./static/env.js"
 // import words from './static/words.js';
 import db from'./configuration/firebase.js'
-import { ref, set, onValue, get } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
-
+import { ref, set, onValue, get, off } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
 
 const wordRef = ref(db, 'words/')
 const declensionRef = ref(db, 'declensions/')
-// console.log(words);
-const nameInput = document.querySelector(".userInput > input")
 
 const app = createApp({
   data() {
@@ -24,19 +21,13 @@ const app = createApp({
       ],
       // 從資料庫抓到的詞彙資料
       words: [],
-      // 使用者的當下輸入
-      userInput: "",
       // 當下要測試的字
       currentWord: null,
       status: "使用者尚未輸入...",
       statusUpload: "尚未輸入",
-      // inputData: {
-      //   singleInputs: [],
-      //   pluralInputs: []
-      // },
       inputIsSelected: false,
       mode: "test", 
-      // 預計使用的新版資料架構
+      // 新版資料架構
       inputs: {
         name: "",
         gender: "",
@@ -50,17 +41,14 @@ const app = createApp({
   watch: {
     "inputs.name": function(newInput, oldInput){
       this.currentWord = this.words.find( word => word.name == newInput ) || null
-      // this.currentWord = 1
-      // 根據有無找到對應的 currentWord 來決定 status 的顯示
-      if(typeof(this.currentWord) == "object" && this.currentWord !== null && this.currentWord.type != ""){
-        this.status = "找到了，試試看！"
-      }else{
-        this.status = "資料庫中沒找到這個字..."
-      }
+      // // this.currentWord = 1
+      // // 根據有無找到對應的 currentWord 來決定 status 的顯示
+      // if(typeof(this.currentWord) == "object" && this.currentWord !== null && this.currentWord.type != ""){
+      //   this.status = "找到了，試試看！"
+      // }else{
+      //   this.status = "資料庫中沒找到這個字..."
+      // }
     },
-  //   words(){
-  //     console.log("改變了")
-  //   }
   },
   methods: {
     toggleMode(now){
@@ -78,22 +66,14 @@ const app = createApp({
         stem,
         single,
         plural 
-      } = this.input
+      } = this.inputs
       let word, declension = {}
-      // word = { name, gender, type, stem }
-      // declension = { name, single, plural }
-      word = {
-        name: this.userInput,
-        gender: this.genderInput,
-        type: this.typeInput,
-        stem: this.stemInput
-      }
-      declension = {
-        name: this.userInput,
-        single: this.singleInputs,
-        plural: this.pluralInputs
-      }
       let nextIndex = this.words.length
+
+      word = { name, gender, type, stem }
+      declension = { name, single, plural }
+
+      // console.log("上傳後的狀況："+this.words, nextIndex);
       let wordRef = ref(db, "/words/" + nextIndex)
       let declensionRef = ref(db, "/declensions/" + nextIndex)
       set(wordRef, word)
@@ -102,8 +82,38 @@ const app = createApp({
         setTimeout(()=>{
           this.statusUpload = "可以繼續新增"
         },1000)
+      }).catch( err =>{
+        console.log(err)
       })
       this.statusUpload = "新增中..."
+    },
+    initData(){
+      let tempR = [] 
+
+      onValue(wordRef, (snapshot)=>{
+        tempR = [] /* 將閉包的 tempR 清空 */
+        let resultArr = snapshot.val()
+        console.log("resultArr", resultArr);
+
+        resultArr.forEach((item) => {
+          tempR.push(item)
+        })
+
+      },{
+        // onlyOnce: true
+      })
+
+      onValue(declensionRef, (snapshot)=>{
+        let resultArr = snapshot.val()
+
+        resultArr.forEach((item, i) => {
+          tempR[i] = {...tempR[i], ...resultArr[i]}
+        })
+
+        this.words = tempR
+      },{
+        // onlyOnce: true
+      })
     }
   },
   computed: {
@@ -131,34 +141,15 @@ const app = createApp({
     //   return true 
     // }
   },
-  created: function(){
-    let tempR = []
-
-    onValue(wordRef, (snapshot)=>{
-      let resultArr = snapshot.val()
-
-      resultArr.forEach((item) => {
-        tempR.push(item)
-      })
-    })
-
-    onValue(declensionRef, (snapshot)=>{
-      let resultArr = snapshot.val()
-
-      resultArr.forEach((item, i) => {
-        tempR[i] = {...tempR[i], ...resultArr[i]}
-      })
-
-      this.words = tempR
-    })
+  created(){
+    this.initData()
   },
+  // mounted(){
+  //   console.log(this.words);
+  // }
 })
 
 app.mount("#app")
-
-nameInput.addEventListener("click", ()=>{
-  console.log("點到了")
-})
 
 $(".userInput > input").focus(()=>{
   app.inputIsSelected = true
--- test.js
+++
@@ -1,8 +0,0 @@
-console.log("etss")
-const rtt = 2
-let nameInput = document.querySelector(".userInput > input")
-console.log(nameInput)
-
-nameInput.addEventListener("click", ()=>{
-  console.log("點到了")
-})
